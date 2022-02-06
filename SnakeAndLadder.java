9e72e6c1a3beed679b4d53c849f9c92594124a2a
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-06 19:26:22 +0530

    UC1 Added :Initial Position of Players 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.snakeandladder;
+
+public class SnakeAndLadder 
+{
+	public static void main(String[] args) 
+	{
+		int position = 0;
+		System.out.println("Initial Position of player starts from " +position);
+	}
+
+}
+//Snake and Ladder game played with single player at start position 0
